bbd7ae8570e15343d8046d2ccff1426c10db1a8d
KeltonKarboviak <kelton.karboviak@gmail.com>
2017-10-03 17:07:53 -0500

    Adding initial files

---
+++ database/seed.sql
@@ -0,0 +1,6 @@
+
+insert into games (asin, title, price) VALUES (asin_t('1234567890'), 'game1', 15.00);
+insert into games (asin, title, price) VALUES (asin_t('0987654321'), 'game2', 18.25);
+insert into games (asin, title, price) VALUES (asin_t('B00QWERTYU'), 'game3', 12.66);
+
+
