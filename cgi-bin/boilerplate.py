8e93ac376d96ba4d9ea467bc85fd2cedf233ad91
Kelton Karboviak <kelton.karboviak@gmail.com>
2017-10-27 00:11:24 -0500

    Added a boilerplate file for python scripts

---
+++ cgi-bin/boilerplate.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python
+
+import cgi
+import cx_Oracle as Oracle
+import json
+import os
+from dotenv import load_dotenv, find_dotenv
+
+
+def main():
+    credentials = {
+        'user': os.environ.get('ORACLE_USER'),
+        'password': os.environ.get('ORACLE_PASSWORD'),
+        'dsn': os.environ.get('ORACLE_HOST'),
+    }
+
+    form = cgi.FieldStorage()
+
+    # Get the customer's id
+    customer_id = form.getvalue('id', '')
+
+    # Grab the asins that were passed in and their corresponding quantities
+    asins = form.getlist('asins[]')
+    qtys = [int(q) for q in form.getlist('qtys[]')]
+
+    status = 'error'
+
+    connection, cursor = None, None
+    try:
+        with Oracle.connect(**credentials) as connection:
+            cursor = connection.cursor()
+
+            # In query we're doing a RIGHT JOIN because we always want to get
+            # the price of the game whether it's in their account already
+            # or not
+            sql = "SELECT * FROM customers"
+
+            cursor.execute(sql)
+
+            results = cursor.fetchall()
+
+        status = 'success'
+    except Oracle.DatabaseError as e:
+        print e
+    finally:
+        if cursor is not None:
+            cursor.close()
+
+        print json.dumps({'status': status})
+    # end finally
+# end def main()
+
+
+if __name__ == '__main__':
+    # Load in environment variables
+    load_dotenv(find_dotenv())
+
+    main()
