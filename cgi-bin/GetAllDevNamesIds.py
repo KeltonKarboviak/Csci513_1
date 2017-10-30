89d4e6ee414d2bc6f58693d2126d763082aab11b
Kelton Karboviak <kelton.karboviak@gmail.com>
2017-10-30 17:18:38 -0500

    Added back-end implementation for fetching all dev names & ids

---
+++ cgi-bin/GetAllDevNamesIds.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+
+import cx_Oracle as Oracle
+import json
+import os
+from dotenv import load_dotenv, find_dotenv
+
+
+def main():
+    credentials = {
+        'user': os.environ.get('ORACLE_USER'),
+        'password': os.environ.get('ORACLE_PASSWORD'),
+        'dsn': os.environ.get('ORACLE_HOST'),
+    }
+
+    developers = []
+
+    status = 'error'
+
+    connection, cursor = None, None
+    try:
+        with Oracle.connect(**credentials) as connection:
+            cursor = connection.cursor()
+
+            sql = """\
+                SELECT *
+                FROM developers
+            """
+
+            cursor.execute(sql)
+
+            results = cursor.fetchall()
+
+            developers = [{'id': d[0], 'name': d[1]} for d in results]
+
+        status = 'success'
+    except Oracle.DatabaseError as e:
+        print e
+    finally:
+        if cursor is not None:
+            cursor.close()
+
+        print json.dumps({'status': status, 'devs': developers})
+    # end finally
+# end def main()
+
+
+if __name__ == '__main__':
+    # Load in environment variables
+    load_dotenv(find_dotenv())
+
+    main()
