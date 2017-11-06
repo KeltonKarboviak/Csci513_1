383eff4ffd82e8f00f08e994cba2fe71d35a2e70
Kelton Karboviak <kelton.karboviak@gmail.com>
2017-11-06 14:29:15 -0600

    Added a js function to build a footer on every page
    main.js:
    + Added a buildFooter() function that will build a footer with a link to
    the website's source on GitHub. This is according to the instructor's
    requirements.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
-$(function () {
+(function ($) {
 
-});
+    buildFooter();
+
+})(jQuery);
+
+function buildFooter() {
+    $('body').append(
+        $('<footer>', {class: 'navbar fixed-bottom navbar-dark bg-dark'}).append(
+            $('<div>', {class: 'container'}).append(
+                $('<a>', {href: '#', class: 'navbar-brand'}),
+                $('<ul>', {class: 'navbar-nav mr-auto'}).append(
+                    $('<li>', {class: 'nav-item'}).append(
+                        $('<a>', {href: 'https://github.com/KeltonKarboviak/CSci513_1', class: 'nav-link'}).append(
+                            $('<span>', {class: 'oi oi-terminal oi-label'}),
+                            document.createTextNode(' View Source')
+                        )
+                    )
+                )
+            )
+        )
+    );
+}
